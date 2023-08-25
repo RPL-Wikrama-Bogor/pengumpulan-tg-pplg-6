f48fba475879b529a8b9b76a1d4af6f1c9c20b23
abduljabar0606 <abd.jabar0306@gmail.com>
2023-08-25 09:58:11 +0700

    tambahFile

---
+++ 12209142-Muhamad Abdul Jabar/algo16.php
@@ -0,0 +1,14 @@
+<?php
+
+for ($i=1; $i <= 50; $i++) {
+
+    if ($i %2 ){  
+        
+        if ($i == 1) {
+            echo "bilangan ganjil $i";
+        }
+        if ($i == 0) {
+            echo "bilangan genap $i";
+        }
+    }
+}
