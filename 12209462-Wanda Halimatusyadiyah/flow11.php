9c2e7c5aa5a2004ab510f4d064f179fc188cd98d
wanda <wandahalimatu91@gmail.com>
2023-09-04 13:47:52 +0700

    wanda

--- 12209462-Wanda Halimatusyadiyah/flow11.php
+++ 12209462-Wanda Halimatusyadiyah/flow11.php
@@ -3,43 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            padding: 50px; /* Adjust padding for smaller screens */
+            background-image: url("https://i.pinimg.com/564x/4a/ab/58/4aab58e0e0bd778350ee5a2f3715b9d7.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+            margin: 0; /* Remove default margin */
+            font-family: sans-serif;
+        }
+
+        #feedback-form {
+            width: 100%; /* Use full width for small screens */
+            max-width: 400px; /* Limit maximum width */
+            margin: 0 auto;
+            background-color: #fcfc;
+            padding: 25px;
+            box-shadow: 1px 4px 10px 1px #aaa;
+            border-radius: 15px;
+        }
+
+        #feedback-form * {
+            box-sizing: border-box;
+        }
+
+        h2 {
+            color: #FE7BE5;
+        }
+
+        input[type="number"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        input[type="submit"] {
+            display: block;
+            width: 100%;
+            background-color: #FE7BE5;
+            color: white;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
+    <title>Responsive Form</title>
 </head>
 <body>
-    <style>
-body{
-  padding:200px;
-  background-image : url("https://i.pinimg.com/564x/4a/ab/58/4aab58e0e0bd778350ee5a2f3715b9d7.jpg");
-  background-repeat: no-repeat;
-  background-size:cover;
-}
-#feedback-form {
-  width: 280px;
-  margin: 0 auto;
-  background-color: #fcfc;
-  padding: 25px;
-  box-shadow: 1px 4px 10px 1px #aaa;
-  font-family: sans-serif;
-  border-radius:15px;
-}
-
-#feedback-form * {
-    box-sizing: border-box;
-}
-
-h2{
-  color:#FE7BE5;  
-}
-
-
-    </style>
-    <form action="" method="post">
     <div id="feedback-form">
-  <h2 class="header">input</h2>
-  <div>
-    <form>
-      <input type="number" name="pegawai" placeholder=""></input>
-        <input type="submit" value="Submit" name="submit">
-    </form>
+        <h2 class="header">Input</h2>
+        <form action="" method="post">
+            <input type="number" name="pegawai" placeholder="Enter a number">
+            <input type="submit" value="Submit" name="submit">
+        </form>
+
 <?php
  $pegawai;
  $golongan;
--- 12209462-Wanda Halimatusyadiyah/flow19.php
+++ 12209462-Wanda Halimatusyadiyah/flow19.php
@@ -4,6 +4,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <body>
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .container {
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            width: 100%;
+            max-width: 400px;
+        }
+
+        h2 {
+            text-align: center;
+            color: #333;
+        }
+
+        label {
+            font-weight: bold;
+            margin-bottom: 5px;
+            color: #555;
+        }
+
+        input[type="number"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+
+        input[type="submit"] {
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            padding: 10px 20px;
+            cursor: pointer;
+            width: 100%;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+    </style>
+    
 <form action="" method="post">
     <h2>Penjualan Tiket Bioskop</h2>
     <label for="vip">Tiket VIP:</label>
