e7a2c6c1bd605450432de424c646b77a7a1c74ff
XyKhairul <muhammadkhairulikhwan@smkwikrama.sch,id>
2023-09-07 09:22:18 +0700

    tambahfile

--- 12209244-Muhammad Khairul Ikhwan/Flowchart/19.php
+++ 12209244-Muhammad Khairul Ikhwan/Flowchart/19.php
@@ -2,6 +2,55 @@
 <html>
 <head>
     <title>No 19</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 20px;
+        }
+        
+        h2 {
+            color: #333;
+        }
+        
+        form {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        }
+        
+        label {
+            display: block;
+            margin-bottom: 10px;
+        }
+        
+        input[type="number"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        
+        input[type="submit"] {
+            background-color: #007BFF;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        
+        input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+        
+        h3 {
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
     
