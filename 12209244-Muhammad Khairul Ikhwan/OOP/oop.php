497515c396eef1d33e95e6b6126af85ad78dd005
XyKhairul <muhammadkhairulikhwan@smkwikrama.sch,id>
2023-09-18 15:04:46 +0700

    tambah

--- 12209244-Muhammad Khairul Ikhwan/OOP/oop.php
+++ 12209244-Muhammad Khairul Ikhwan/OOP/oop.php
@@ -1,184 +1,164 @@
 <!DOCTYPE html>
-<html>  
+<html lang="en">
 <head>
-    <title>Form Pembelian Bahan Bakar</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bahan Bakar</title>
     <style>
         body {
             font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
+            background-color: #f0f0f0;
             margin: 0;
-            flex-direction: column;
-            background-color: #f4f4f4;
+            padding: 0;
         }
 
-        .container {
-            background-color: #fff;
-            border-radius: 5px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            padding: 20px;
+        h2 {
             text-align: center;
+            margin-top: 20px;
+            color: #007bff; 
         }
 
-        h1 {
-            margin-bottom: 20px;
+        table {
+            margin: 0 auto;
+            border-collapse: collapse;
+            width: 80%;
+            max-width: 600px;
+            background-color: #fff; 
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+            border: 1px solid #ccc;
         }
 
-        form {
-            display: inline-block;
+        table td {
+            padding: 10px;
+            text-align: right;
+        }
+
+        table select, table input[type="number"], table button {
+            padding: 5px;
+            font-size: 16px;
+            width: 100%;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+        }
+
+        table select {
             text-align: left;
         }
 
-        label {
-            display: block;
-            margin-bottom: 8px;
+        table tr:nth-child(odd) {
+            background-color: #f2f2f2;
         }
 
-        input[type="text"],
-        select {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            margin-bottom: 15px;
+        button[type="submit"] {
+            background-color: #007bff;
+            color: white;
+            cursor: pointer;
+            transition: background-color 0.3s;
+            border: none; 
         }
 
-        input[type="submit"] {
-        background-color: maroon; 
-        color: #fff;
-        padding: 10px 20px;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-        }   
-
-        input[type="submit"]:hover {
-        background-color: darkred; 
+        button[type="submit"]:hover {
+            background-color: #0056b3;
         }
 
-
-        .output-container {
-            background-color: #fff;
-            border-radius: 5px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            padding: 20px;
-            text-align: center;
+        .result {
+            width: 500px;
             margin-top: 20px;
-            display: <?php echo $showForm ? 'none' : 'block'; ?>;
+            padding: 10px;
+            background-color: #007bff;
+            color: white;
+            border-radius: 5px;
         }
     </style>
 </head>
 <body>
-    <div class="container">
-        <?php
-        class FuelPurchaseForm
-        {
-            private $showForm = true;
-
-            public function __construct()
-            {
-                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                    $this->handleFormSubmission();
-                }
-                $this->renderForm();
-            }
-
-            private function handleFormSubmission()
-            {
-                $jumlahLiter = $_POST['jumlahLiter'];
-                $tipeBahanBakar = $_POST['tipeBahanBakar'];
-
-                // Harga bahan bakar
-                $hargaShellSuper = 15420.00;
-                $hargaShellVPower = 16130.00;
-                $hargaShellVPowerDiesel = 18310.00;
-                $hargaShellVPowerNitro = 16510.00;
-
-                // Hitung total harga
-                $totalHarga = 0;
-
-                switch ($tipeBahanBakar) {
-                    case 'Shell Super':
-                        $totalHarga = $hargaShellSuper * $jumlahLiter;
-                        break;
-                    case 'Shell V-Power':
-                        $totalHarga = $hargaShellVPower * $jumlahLiter;
-                        break;
-                    case 'Shell V-Power Diesel':
-                        $totalHarga = $hargaShellVPowerDiesel * $jumlahLiter;
-                        break;
-                    case 'Shell V-Power Nitro':
-                        $totalHarga = $hargaShellVPowerNitro * $jumlahLiter;
-                        break;
-                    default:
-                        echo "Tipe bahan bakar tidak valid";
-                        exit;
-                }
-
-                // Hitung PPN
-                $ppn = 10;
-                $ppnAmount = ($ppn / 100) * $totalHarga;
-                $this->showForm = false;
-                $this->renderResult($jumlahLiter, $tipeBahanBakar, $totalHarga, $ppnAmount);
-            }
-
-            private function renderForm()
-            {
-                if ($this->showForm) {
-                    ?>
-                    <h1>Form Pembelian Bahan Bakar</h1>
-                    <form action="" method="POST">
-                        <label for="jumlahLiter">Masukkan Jumlah Liter:</label>
-                        <input type="text" name="jumlahLiter" id="jumlahLiter" required>
-                        <br><br>
-                        <label for="tipeBahanBakar">Pilih Tipe Bahan Bakar:</label>
-                        <select name="tipeBahanBakar" id="tipeBahanBakar" required>
-                            <option value="Shell Super">Shell Super</option>
-                            <option value="Shell V-Power">Shell V-Power</option>
-                            <option value="Shell V-Power Diesel">Shell V-Power Diesel</option>
-                            <option value="Shell V-Power Nitro">Shell V-Power Nitro</option>
-                        </select>
-                        <br><br>
-                        <input type="submit" value="Beli">
-                    </form>
-                    <?php
-                }
-            }
-
-            private function renderResult($jumlahLiter, $tipeBahanBakar, $totalHarga, $ppnAmount)
-            {
-                ?>
-                <div class="output-container">
-                    <h1>Hasil Pembelian Bahan Bakar</h1>
-                    <p>-------------------------------------------------</p>
-                    <p>Anda membeli bahan bakar minyak tipe <?php echo $tipeBahanBakar; ?></p>
-                    <p>Dengan jumlah: <?php echo $jumlahLiter; ?> Liter</p>
-                    <p>Total yang harus anda bayar Rp. <?php echo number_format($totalHarga + $ppnAmount, 2); ?></p>
-                    <p>-------------------------------------------------</p>
-                </div>
-                <h1>Form Pembelian Bahan Bakar</h1>
-                <form action="" method="POST">
-                    <label for="jumlahLiter">Masukkan Jumlah Liter:</label>
-                    <input type="text" name="jumlahLiter" id="jumlahLiter" required>
-                    <br><br>
-                    <label for="tipeBahanBakar">Pilih Tipe Bahan Bakar:</label>
-                    <select name="tipeBahanBakar" id="tipeBahanBakar" required>
+    <h2>Bahan Bakar</h2>
+    <table>
+        <form action="" method="post">
+            <tr>
+                <td>Masukan Jumlah Liter</td>
+                <td>:</td>
+                <td><input type="number" name="liter" required></td>
+            </tr>
+            <tr>
+                <td>Pilih Tipe Bahan Bakar</td>
+                <td>:</td>
+                <td>
+                    <select name="bahanBakar" required>
+                        <option hidden>Pilih bahan bakar</option>
                         <option value="Shell Super">Shell Super</option>
                         <option value="Shell V-Power">Shell V-Power</option>
                         <option value="Shell V-Power Diesel">Shell V-Power Diesel</option>
                         <option value="Shell V-Power Nitro">Shell V-Power Nitro</option>
                     </select>
-                    <br><br>
-                    <input type="submit" value="Beli">
-                </form>
-                <?php
-            }
-        }
-
-        new FuelPurchaseForm();
-        ?>
-    </div>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><button type="submit" name="button">Beli</button></td>
+            </tr>
+        </form>
+    </table>
 </body>
 </html>
+<?php   if (isset($_POST['button'])) { ?>
+<center>
+<div class="result">
+<?php
+
+class Shell{
+    
+    public  $Shell_Super = 15420,
+            $Shell_V_Power = 16130,
+            $Shell_V_Power_Diesel = 18310,
+            $Shell_V_Power_Nitro = 16510;
+
+    public $PPN =10/100;
+
+}
+
+class Beli extends Shell{
+
+    public function hitung($harga, $jumlah)
+    {
+        $hAkhir = $harga * $jumlah + ($harga * $jumlah * $this->PPN);
+        echo "Anda Membeli Bahan Bakar Minyak ". $_POST['bahanBakar'] . "<br>";
+        echo "Dengan Jumlah : $jumlah Liter<br>";
+        echo "Total Yang Harus Anda bayar : Rp." . number_format($hAkhir, 0, ',', '.'). ",-";
+    }
+}
+
+
+if(isset($_POST['button'])){
+
+    $shell = new Beli();
+
+    $bahanBakar = $_POST['bahanBakar'];
+    $jumlah = $_POST['liter'];
+
+    switch($bahanBakar){
+        case "Shell Super" : 
+            $harga = $shell->Shell_Super;
+            break;
+        case "Shell V-Power" : 
+            $harga = $shell->Shell_V_Power;
+            break;
+        case "Shell V-Power Diesel" : 
+            $harga = $shell->Shell_V_Power_Diesel;
+            break;
+        case "Shell V-Power Nitro" : 
+            $harga = $shell->Shell_V_Power_Nitro;
+            break;
+        default : 
+            $harga = 0;
+            break;
+    }
+
+    echo $shell->hitung($harga, $jumlah);
+}
+}
+?>
+</div>
+</center>
