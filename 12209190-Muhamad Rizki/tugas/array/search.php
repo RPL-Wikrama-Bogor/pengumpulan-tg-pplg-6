b129bb55be31024091c5fd293b2aba4ae3787d71
naelle0 <salipadanglili@gmail.com>
2023-09-18 14:45:40 +0700

    12209190

--- 12209190-Muhamad Rizki/tugas/array/search.php
+++ 12209190-Muhamad Rizki/tugas/array/search.php
@@ -68,14 +68,27 @@
 }
     ?>
     <?php
-    if(isset($_POST['submit'])){
-        $nama = $_POST['nama'];
-        foreach($names as $key => $data_siswa){
-            if($nama == $data_siswa['nama']){
-               
-            }
-        }
-    }
-    ?>
+   
+   if(isset($_POST['submit'])){
+       $nama = $_POST['nama'];
+       $umur_di_bawah_17 = false; 
+       
+   foreach($names as $key => $data_siswa){
+       if($nama == $data_siswa['nama']){
+           if($data_siswa['umur'] <= 17){
+               $umur_di_bawah_17 = true;
+               break; 
+           }
+               echo "Nama: $nama<br>";
+               echo "Umur: {$data_siswa['umur']}<br>";
+       }
+   }
+   
+   if($umur_di_bawah_17){
+       echo "Umur Anda di bawah 17 tahun";
+   }
+}
+?>
+
 </body>
 </html>
