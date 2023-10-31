ef96fff6b07288794e5713b4d48393391ab705ee
azkaafr <muhammadazkafathurrahman@smkwikrama.sch.id>
2023-10-31 14:32:05 +0700

    12209211

--- 12209211-Muhammad Azka Fathurrahman/vue/vue-project/src/App.vue
+++ 12209211-Muhammad Azka Fathurrahman/vue/vue-project/src/App.vue
@@ -1,108 +1,52 @@
 <template>
-
-     <p>----------data bidang---------</p>
-    {{ nama }}
-    {{ number }}
-    <div v-html="rombel"></div>
-
-    <p>----------data bidang---------</p>
-    <button if:disabled="nonaktif">tombol</button>
-    <h1 v-bind="property">F</h1>
-    <input type="typeinput">
-
-    <p>--------data bidang-----------</p>
-    {{ count == 0 ? count + 1 : count +2}}
- 
-    <p>----------v------------</p>
-
-    <button v-if="show">Tombol</button>
-    <button v-show="show">submit</button>
-
-         <div v-if="count==1">
-            number 1
-         </div>
-        <div v-else="count==1">
-            number 2
-        </div>
-        <div v-else>
-            lebih dari 2
-        </div>
-
-        <p>-----------v----------</p>
-        <input :type="typeinput">
-        <button @click="showpassword">show password</button>
-        <!-- <button @click="counternumber">{{ counterbutton }}</button>
-        <button @click="countcomputed">computed{{ numbercomputed }}</button> -->
-        <p>-----------compputted dan method------------</p>
-        <ul>
-            <li class="fs" :class="{active:isActive, fs40px:isActive}"> 
-                F
-            </li>
-        </ul>
-        <button @click="ubahwarna">ubah warna</button>
-        <ul>
-            <li v-for="(item, index) in daftarrombel">{{ index+1 }} {{ item }}</li>
-        </ul>
-        <p>----------v-model------------</p>
-            <input type="text" v-model="inputkelas">
-            {{ inputkelas }}
-</template>
-<script>
-export default{
-    data(){
-        return{
-            inputkelas:'',
-            daftarrombel: ['RPL 1', 'RPL 2', 'RPL 3'],
-            conterbutton: 0,
-            numbercomputed: 0,
-            show: false,
-            count: 0,
-            nama: 'Azka',
-            number: '68',
-            rombel: '<h1> RPL </h1>',
-            nonaktif: false,
-            property: {
-                id: 1,
-                class:'color'
-            },
-            typeinput: 'password',
-            isActive: true
+    <div>
+      <input type="text" placeholder="masukan nama" v-model="formDataKelas.nama">
+      <br>
+      <input type="text" placeholder="masukan kelas" v-model="formDataKelas.Kelas">
+      <br>
+      <button @click="tambahKelas">tambah siswa</button>
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>nama</th>
+          <th>Kelas</th>
+          <th>Aksi</th>
+        </tr>
+        <tr v-for="(murid, index) in dataKelas" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ murid.nama }}</td>
+          <td>{{ murid.Kelas }}</td>
+          <td><button @click="hapusDataKelas(murid.id)">hapus</button></td>
+        </tr>
+      </table>
+    </div>
+  </template>
+  <script>
+  export default {
+    data() {
+      return{
+        dataKelas: [{
+          nama: 'Azka',
+          Kelas: XI
+        }],
+        formDataKelas:{
+          nama:'',
+          Kelas:'',
         }
+      }
     },
-    methods : {
-        counternumber(){
-            this.counterbutton += 1;
-        },
-        showpassword(){
-            if(this.typeinput == 'password'){
-            this.typeinput = 'text'
-            }else{
-                this.typeinput = 'password'
-            }
-        }, ubahwarna(){
-            if(this.isActive){
-                this.isActive = false;
-            }else{
-                this.isActive = true;
-            }
+    methods: {
+      tambahKelas(){
+        this.formDataKelas.id = Date.now();
+  
+        this.dataKelas.push(this.formDataKelas);
+        this.formDataKelas = {
+          nama: '',
+          Kelas: '',
         }
-    },
-    computed: {
-        countcompoted(){
-
-        }
+      },
+      hapusDataKelas(id){
+        this.dataKelas = this.dataKelas.filter(item => item.id != id);
+      }
     }
-}
-</script>
-
-<style>
-.color{
-    color: grey;
-}
-.fs40px{ 
-    font-size:40px;
-}
-.active{
-  color:green;
-}
-</style>
+  }
