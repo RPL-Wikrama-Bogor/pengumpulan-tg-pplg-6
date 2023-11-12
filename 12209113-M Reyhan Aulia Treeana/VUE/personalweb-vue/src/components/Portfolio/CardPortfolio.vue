0f801977fd9495f3ccaeb0f3974a40bbf444f36d
ryhanauliaatreeee <inireyhann03@gmail.com>
2023-11-12 07:39:28 +0700

    Penambahan materi vue

---
+++ 12209113-M Reyhan Aulia Treeana/VUE/personalweb-vue/src/components/Portfolio/CardPortfolio.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="row row-cols-1 row-cols-md-6 g-4">
+            <div class="col">
+              <div class="card">
+                <img src="img/magic.jpg" class="card-img-top" style="width: 250px;" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+    </div>
+</template>
