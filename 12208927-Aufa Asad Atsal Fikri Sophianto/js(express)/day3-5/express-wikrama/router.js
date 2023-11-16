cc662779934c3a7b86e1f7b87c74ac5350f443ef
auliyasalsabila <auliyasalsa30@gmail.com>
2023-11-17 06:55:45 +0700

    industry

---
+++ 12208927-Aufa Asad Atsal Fikri Sophianto/js(express)/day3-5/express-wikrama/router.js
@@ -0,0 +1,47 @@
+const handle = (req, res) => {
+    res.send("<h1>Welcome to Express</h1>");
+};
+const handleHome = (req, res) => {
+    res.send("<h1>Welcome to Home</h1>");
+};
+const handleAbout = (req, res) => {
+    res.send("<h1>Welcome to About</h1>");
+};
+const handleContact = (req, res) => {
+    res.send("<h1>Welcome to Contact</h1>");
+};
+const handleNews = (req, res) => {
+    res.send("<h1>Welcome to News</h1>");
+};
+
+module.exports = 
+{
+    handle,
+    handleHome,
+    handleAbout,
+    handleContact,
+    handleNews
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
