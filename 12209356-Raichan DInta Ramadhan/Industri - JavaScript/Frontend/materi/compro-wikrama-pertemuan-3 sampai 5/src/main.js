afa8a0518e90203fd0eec6f629923e38c57f5c69
raichan07 <raichandintar@gmail.com>
2023-11-19 15:38:09 +0700

    tugas expres-js

---
+++ 12209356-Raichan DInta Ramadhan/Industri - JavaScript/Frontend/materi/compro-wikrama-pertemuan-3 sampai 5/src/main.js
@@ -0,0 +1,38 @@
+import "@/assets/style.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/pages/Home.vue";
+import Portfolio from "@/pages/Portfolio.vue";
+import Blog from "@/pages/Blog.vue";
+import Service from "@/pages/Service.vue";
+import Contact from "@/pages/Contact.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/Portfolio",
+    component: Portfolio,
+  },
+  {
+    path: "/blog",
+    component: Blog,
+  },
+  {
+    path: "/service",
+    component: Service,
+  },
+  {
+    path: "/contact",
+    component: Contact,
+  },
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
